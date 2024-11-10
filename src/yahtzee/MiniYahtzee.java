44c184e10d575fb5cae95be0bae9fa22e3817462
rishivardhan <vardhan.rishi@gmail.com>
2024-11-10 19:14:55 +0100

    refctored code

--- src/yahtzee/MiniYahtzee.java
+++ src/yahtzee/MiniYahtzee.java
@@ -1,56 +1,50 @@
 package yahtzee;
 
+import java.util.Map;
+
 class MiniYahtzee {
 
+    private static final Map<String, Integer> NOMINATION_TO_TARGET = Map.of(
+            "ones", 1,
+            "twos", 2,
+            "threes", 3
+    );
+
     public static void main(String[] args) {
-        score(args[0], args[1], args[2], args[3]);
+        int score = score(args[0], args[1], args[2], args[3]);
+        System.out.println("Score: " + score);
     }
 
-    public static int score(String nomination, String d1, String d2, String d3) {
-        int score = 0;
-        int first = Integer.parseInt(d1);
+    public static int score(String nomination, String... dice) {
+        Integer targetNumber = NOMINATION_TO_TARGET.get(nomination);
 
-        if (nomination.equals("ones")) {
-            score = new Max().ones(first, Integer.parseInt(d2), Integer.parseInt(d3));
-        }
-        if (nomination.equals("twos")) {
-            score = new Max().twos(first, Integer.parseInt(d2), Integer.parseInt(d3));
-        }
-        if (nomination.equals("threes")) {
-            score = new Max().threes(first, Integer.parseInt(d2), Integer.parseInt(d3));
+        if (targetNumber == null) {
+            System.out.println("Invalid nomination: " + nomination);
+            return 0; // Return 0 if the nomination is not recognized
         }
 
-        return score;
+        // Parse dice values and pass to DiceCounter
+        int[] diceValues = new int[dice.length];
+        for (int i = 0; i < dice.length; i++) {
+            diceValues[i] = Integer.parseInt(dice[i]);
+        }
+
+        DiceCounter counter = new DiceCounter(targetNumber);
+        return counter.calculateScore(diceValues);
     }
 }
 
-class Ones {
-    int ones(int d1, int d2, int d3) {
-        int sum = 0;
-        if (d1 == 1) sum++;
-        if (d2 == 1) sum++;
-        if (d3 == 1) sum++;
-        return sum;
-    }
-}
+class DiceCounter {
+    private final int target;
 
-class Twos extends Ones {
-    int twos(int d1, int d2, int d3) {
-        int sum = 0;
-        if (d1 == 2) sum += 2;
-        if (d2 == 2) sum += 2;
-        if (d3 == 2) sum += 2;
-        return sum;
+    public DiceCounter(int target) {
+        this.target = target;
     }
-}
 
-class Max extends Twos {
-    int threes(int d1, int d2, int d3) {
-        int s;
-        s = 0;
-        if (d1 == 3) s += 3;
-        if (d2 == 3) s += 3;
-        if (d3 == 3) s += 3;
-        return s;
+    public int calculateScore(int... dice) {
+        // Use streams for a concise summation of matching dice values
+        return (int) java.util.Arrays.stream(dice)
+                .filter(d -> d == target)
+                .count() * target;
     }
 }
